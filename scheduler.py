159713d3b3c2cea0f362bc7cb79fcbac7aa9f319
ChiachunKuo <165041655+ChiachunKuo@users.noreply.github.com>
2026-04-23 20:03:36 +0800

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -45,5 +45,5 @@ def send_reminder():
 
 def start_scheduler():
     scheduler = BackgroundScheduler(timezone=pytz.timezone('Asia/Taipei'))
-    scheduler.add_job(send_reminder, 'cron', hour=20, minute=00)
+    scheduler.add_job(send_reminder, 'cron', hour=15, minute=00)
     scheduler.start()
